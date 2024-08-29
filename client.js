0db3bad0a85a9f6742aff566322a1c73e6087477
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2024-08-28 23:42:13 -0600

    Input Algorithm

--- client.js
+++ client.js
@@ -1,6 +1,12 @@
 const { client } = require('@xmpp/client');
-const { triggerFloodingAction, handleFloodingMessage } = require('./algorithms/Flooding');
-const { getNodeCredentials } = require('./inputHandler');
+const { triggerFloodingAction, handleFloodingMessage, handleLinkStateMessage } = require('./algorithms/Flooding');
+const { getNodeCredentials, promptForAction } = require('./inputHandler');
+
+let xmpp;
+let neighbors = {
+    neighbor1: { jid: 'lem21469-test@alumchat.lol/a1b2c3' },
+    neighbor2: { jid: 'neighbor2@alumchat.lol/a1b2c3' }
+};
 
 getNodeCredentials((err, nodeData) => {
     if (err) {
@@ -11,57 +17,34 @@ getNodeCredentials((err, nodeData) => {
     const username = nodeData.user;
     const password = nodeData.pass;
 
-    const xmpp = client({
+    xmpp = client({
         service: 'ws://alumchat.lol:7070/ws/',
         domain: 'alumchat.lol',
         resource: 'a1b2c3',
-        username: username.split('@')[0], // Asumiendo que el usuario es 'nombre@dominio'
+        username: username.split('@')[0],
         password: password,
     });
 
-    let neighbors = {
-        neighbor1: { jid: 'lem21469-test@alumchat.lol/a1b2c3' },
-        neighbor2: { jid: 'neighbor2@alumchat.lol/a1b2c3' }
-    };
-    let routingTable = {}; // Tabla de enrutamiento
-
-    // Event listener for when the client connects to the server
+    // Initialize XMPP client
     xmpp.on('online', async (address) => {
         console.log(`Connected as ${address.toString()}`);
-        
-    });
+        promptForAction((action) => {
 
+            if (action === "flood") {
+                triggerFloodingAction(xmpp, neighbors);
 
-    // Listen for standard input from the terminal (e.g., typing 'flood' to trigger the Flooding)
-    process.stdin.on('data', (data) => {
-        const input = data.toString().trim();
+            } else if (action === "LSR") {
+                // Agregar llamada a LSR
 
-        if (input === 'flood') {
-            // Trigger the Flooding action
-            triggerFloodingAction(xmpp, neighbors);
-        }
-    });
-
-    xmpp.on('stanza', (stanza) => {
-        if (stanza.is('message')) {
-            // Parse the message to determine its type
-            const message = JSON.parse(stanza.getChild("body").text());
-
-            if (message.type === "flooding") {
-                handleFloodingMessage(stanza, xmpp, neighbors);
-            } else if (message.type === "linkstate") {
-                handleLinkStateMessage(stanza, xmpp, neighbors);
             } else {
-                console.log(`Unknown message type: ${message.type}`);
+                console.log(`Unknown command: ${action}`);
             }
-        }
+        });
     });
 
     xmpp.on('error', (err) => {
         console.error(err);
     });
 
-    // Función para inicializar la conexión XMPP
     xmpp.start().catch(console.error);
-
 });
