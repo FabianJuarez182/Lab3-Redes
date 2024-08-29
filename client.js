7fba98933c8617bd0c9d24991ecbff6641f58895
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2024-08-29 00:01:20 -0600

    feat: link state routing

--- client.js
+++ client.js
@@ -2,14 +2,32 @@ const { client } = require("@xmpp/client");
 const {
   triggerFloodingAction,
   handleFloodingMessage,
-  handleLinkStateMessage,
 } = require("./algorithms/Flooding");
+const { triggerLSRAction, handleLSRMessage } = require("./algorithms/LSR");
 const { getNodeCredentials, promptForAction } = require("./inputHandler");
 
 let xmpp;
 let neighbors = {
   neighbor1: { jid: "lem21469-test@alumchat.lol/a1b2c3" },
   neighbor2: { jid: "neighbor2@alumchat.lol/a1b2c3" },
+};
+let routingTable = {
+  "gom21429@alumchat.lol/a1b2c3": {
+    "jdgomezv1@alumchat.lol/a1b2c3": 1,
+    "jdgomezv2@alumchat.lol/a1b2c3": 2,
+  },
+  "jdgomezv1@alumchat.lol/a1b2c3": {
+    "jdgomezv2@alumchat.lol/a1b2c3": 1,
+    "jdgomezv3@alumchat.lol/a1b2c3": 3,
+  },
+  "jdgomezv2@alumchat.lol/a1b2c3": {
+    "jdgomezv1@alumchat.lol/a1b2c3": 2,
+    "jdgomezv3@alumchat.lol/a1b2c3": 3,
+  },
+  "jdgomezv3@alumchat.lol/a1b2c3": {
+    "jdgomezv1@alumchat.lol/a1b2c3": 2,
+    "jdgomezv2@alumchat.lol/a1b2c3": 3,
+  },
 };
 
 getNodeCredentials((err, nodeData) => {
@@ -35,12 +53,28 @@ getNodeCredentials((err, nodeData) => {
     promptForAction((action) => {
       if (action === "flood") {
         triggerFloodingAction(xmpp, neighbors);
-      } else if (action === "LSR") {
+      } else if (action === "lsr") {
         // Agregar llamada a LSR
+        triggerLSRAction(xmpp, routingTable);
       } else {
         console.log(`Unknown command: ${action}`);
       }
     });
+  });
+
+  xmpp.on("stanza", (stanza) => {
+    if (stanza.is("message")) {
+      // Parse the message to determine its type
+      const message = JSON.parse(stanza.getChild("body").text());
+
+      if (message.type === "flooding") {
+        handleFloodingMessage(stanza, xmpp, neighbors);
+      } else if (message.type === "lsr") {
+        handleLSRMessage(stanza, xmpp, routingTable);
+      } else {
+        console.log(`Unknown message type: ${message.type}`);
+      }
+    }
   });
 
   xmpp.on("error", (err) => {
--- inputHandler.js
+++ inputHandler.js
@@ -1,30 +1,38 @@
-const readline = require('readline');
-const fs = require('fs');
+const readline = require("readline");
+const fs = require("fs");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const nodes = JSON.parse(fs.readFileSync('./maps/nodes.json', 'utf8'));
+const nodes = JSON.parse(fs.readFileSync("./maps/nodes.json", "utf8"));
 
 const getNodeCredentials = (callback) => {
-    rl.question('Please enter the node you would like to use (e.g., A, B, C, D, E): ', (node) => {
-        const nodeData = nodes.nodes[node];
-        if (nodeData) {
-            callback(null, nodeData);
-        } else {
-            console.error('Invalid node selection. Please restart the program and select a valid node.');
-            rl.close();
-            process.exit(1);
-        }
-    });
+  rl.question(
+    "Please enter the node you would like to use (e.g., A, B, C, D, E): ",
+    (node) => {
+      const nodeData = nodes.nodes[node];
+      if (nodeData) {
+        callback(null, nodeData);
+      } else {
+        console.error(
+          "Invalid node selection. Please restart the program and select a valid node."
+        );
+        rl.close();
+        process.exit(1);
+      }
+    }
+  );
 };
 
 const promptForAction = (callback) => {
-    rl.question('Please enter the command (e.g., "flood" or "LSR" to trigger the corresponding algorithm): ', (action) => {
-        callback(action);
-    });
+  rl.question(
+    'Please enter the command (e.g., "flood" or "lsr" to trigger the corresponding algorithm): ',
+    (action) => {
+      callback(action);
+    }
+  );
 };
 
 module.exports = { getNodeCredentials, promptForAction };
